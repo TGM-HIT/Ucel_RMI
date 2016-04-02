8e61971b3c74cfe22212aa46b429f7b704524536
JohannesUcel <u.johannes@hotmail.com>
2016-04-02 13:13:26 +0200

    Calculation Command

--- rmiCommandPattern/src/server/commands/CalculationCommand.java
+++ rmiCommandPattern/src/server/commands/CalculationCommand.java
@@ -1,17 +1,32 @@
 package server.commands;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.rmi.RemoteException;
+
 import calculation.Calculation;
+import callback.Callback;
 
 public class CalculationCommand implements Command, Serializable {
 
 	private static final long serialVersionUID = 3202369269194172790L;
 	private Calculation calc;
+	private Callback<BigDecimal> callbackCalc;
+
+	public CalculationCommand(Calculation calc, Callback<BigDecimal> callbackCalc) {
+		this.calc = calc;
+		this.callbackCalc = callbackCalc;
+	}
 
 	@Override
 	public void execute() {
 		System.out.println("CalculationCommand called!");
 		calc.calculate();
-		calc.getResult();
+		try {
+			callbackCalc.receiveValue(calc.getResult());
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
