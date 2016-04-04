83fe4b8e59ce1e1f14aee9754dc4ae5248e80a6f
JohannesUcel <u.johannes@hotmail.com>
2016-04-04 10:18:15 +0200

    Java Documentation

--- rmiCommandPattern/src/remoteService/DoSomethingService.java
+++ rmiCommandPattern/src/remoteService/DoSomethingService.java
@@ -5,8 +5,23 @@ import java.rmi.RemoteException;
 
 import server.commands.Command;
 
+/**
+ * Dieses Interface gibt eine Methode vor, in der ein Befehl ausgeführt werden
+ * kann
+ * 
+ * @author Johannes Ucel
+ * @version 03. Apr. 2016
+ *
+ */
 public interface DoSomethingService extends Remote {
 
+	/**
+	 * Diese Methode führt ein Befehl (Command) aus
+	 * 
+	 * @param c
+	 *            Ein Befehl
+	 * @throws RemoteException
+	 */
 	public void doSomething(Command c) throws RemoteException;
-	
+
 }
