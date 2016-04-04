288c200dcf7554be67b06960a159b17532af9f31
JohannesUcel <u.johannes@hotmail.com>
2016-04-04 10:08:13 +0200

    Java Documentation

--- rmiCommandPattern/src/calculation/Calculation.java
+++ rmiCommandPattern/src/calculation/Calculation.java
@@ -1,7 +1,26 @@
 package calculation;
 
+import java.math.BigDecimal;
+
+/**
+ * Dieses Interface gibt eine Methode zum Berechnen und eine zum Zurückgeben des
+ * Ergebnisses vor
+ * 
+ * @author Johannes Ucel
+ * @version 03. Apr. 2016
+ *
+ */
 public interface Calculation {
 
+	/**
+	 * Berechnet PI
+	 */
 	public void calculate();
-	public void getResult();
+
+	/**
+	 * Gibt das Ergebnis zurück
+	 * 
+	 * @return gibt das Ergebnis als BigDecimal zurück
+	 */
+	public BigDecimal getResult();
 }
